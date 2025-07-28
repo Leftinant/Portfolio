197800f307a23b800f0a15d62e5a5c3d86757666
leftinant <benjaminyoungner@gmail.com>
2025-07-28 14:01:38 +0300

    new commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className='max-w-screen-2xl container md:px-40 px-10 fixed mx-5 top-0 left-0 right-0 z-99'>
+    <div className='max-w-screen-2xl container md:px-40 px-10 fixed mx-5 top-0 left-0 right-0 z-99 justify-center items-center'>
       <div className='navbar rounded-4xl border-b-3 bg-black/50 backdrop-blur-md'>
         <div className='navbar-start'>
           <div className='dropdown'>
